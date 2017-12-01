12aeda7e535605ab216f2796daa25e042429deda
whyyao <yuanyao.us@gmail.com>
2017-11-30 16:55:08 -0800

    text recon sample

--- app/src/main/java/com/whyyao/scanandsplit/MainActivity.java
+++ app/src/main/java/com/whyyao/scanandsplit/MainActivity.java
@@ -1,13 +1,65 @@
 package com.whyyao.scanandsplit;
 
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.util.StringBuilderPrinter;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mRead;
+    private TextView mText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindView();
+        init();
     }
+
+
+    private void bindView(){
+        mText = (TextView)findViewById(R.id.text_test);
+        mRead = (Button) findViewById(R.id.button_read);
+    }
+
+    private void init(){
+        final Bitmap bitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.sample);
+        mRead.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+                if (!textRecognizer.isOperational()) {
+                  Log.e("ERROR", "Dependency not avaliable");
+                }else{
+                    Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                    SparseArray<TextBlock> items = textRecognizer.detect(frame);
+                    StringBuilder stringBuilder = new StringBuilder();
+                    for (int i = 0; i < items.size(); ++i) {
+                        TextBlock item = items.valueAt(i);
+                        stringBuilder.append(item.getValue());
+                        stringBuilder.append("\n");
+                    }
+                    mText.setText(stringBuilder);
+                }
+            }
+        });
+    }
+
+
 }
