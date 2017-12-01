a3e5431ee35b932d567f8f0c2a53bb05075f2309
yuanyao <yuanyao.us@gmail.com>
2017-11-30 19:31:44 -0800

    scaning branch

--- app/src/main/java/com/whyyao/scanandsplit/MainActivity.java
+++ app/src/main/java/com/whyyao/scanandsplit/MainActivity.java
@@ -1,9 +1,17 @@
 package com.whyyao.scanandsplit;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,6 +19,7 @@ import android.util.SparseArray;
 import android.util.StringBuilderPrinter;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,9 +27,16 @@ import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
 
+import java.io.File;
+import java.io.IOException;
+
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Button mRead;
+    private final int PERMISSIONS_REQUEST_CAMERA = 0;
+    private final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageButton mCam;
     private TextView mText;
 
     @Override
@@ -33,33 +49,74 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void bindView(){
-        mText = (TextView)findViewById(R.id.text_test);
-        mRead = (Button) findViewById(R.id.button_read);
+        mCam = (ImageButton) findViewById(R.id.cam_button);
     }
 
     private void init(){
-        final Bitmap bitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.sample);
-        mRead.setOnClickListener(new View.OnClickListener(){
-
+        mCam.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
-                if (!textRecognizer.isOperational()) {
-                  Log.e("ERROR", "Dependency not avaliable");
-                }else{
-                    Frame frame = new Frame.Builder().setBitmap(bitmap).build();
-                    SparseArray<TextBlock> items = textRecognizer.detect(frame);
-                    StringBuilder stringBuilder = new StringBuilder();
-                    for (int i = 0; i < items.size(); ++i) {
-                        TextBlock item = items.valueAt(i);
-                        stringBuilder.append(item.getValue());
-                        stringBuilder.append("\n");
-                    }
-                    mText.setText(stringBuilder);
-                }
+            public void onClick(View view){
+                lunchCam();
             }
         });
     }
 
+    private void lunchCam(){
+        //check camera permission
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) == PERMISSION_GRANTED) {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }else{
+            //else request permission to camera
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSIONS_REQUEST_CAMERA);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            scanText(imageBitmap);
+        }
+    }
+
+    private SparseArray<TextBlock> scanText(Bitmap bitmap){
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+        SparseArray<TextBlock> items = null;
+        if (!textRecognizer.isOperational()) {
+            Log.e("ERROR", "Dependency not available");
+        }else{
+            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+            items = textRecognizer.detect(frame);
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < items.size(); ++i) {
+                TextBlock item = items.valueAt(i);
+                stringBuilder.append(item.getValue());
+                stringBuilder.append("\n");
+            }
+            Toast.makeText(this, stringBuilder, Toast.LENGTH_LONG).show();
+        }
+        return items;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_CAMERA: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    lunchCam();
+                } else {
+                    //TODO: show a snack bar indicating purpose of cam permission
+                }
+                return;
+            }
+        }
+    }
+
+
 
 }
+
