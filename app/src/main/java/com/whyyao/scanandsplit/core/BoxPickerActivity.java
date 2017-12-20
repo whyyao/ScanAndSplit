2ff6b0426964c6e6ca8a3ba367a291a8eaf59957
Chandler <chan4est@gmail.com>
2017-12-20 00:29:29 -0800

    Mistake on commit

--- app/src/main/java/com/whyyao/scanandsplit/core/BoxPickerActivity.java
+++ app/src/main/java/com/whyyao/scanandsplit/core/BoxPickerActivity.java
@@ -3,10 +3,12 @@ package com.whyyao.scanandsplit.core;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -16,9 +18,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.util.SparseArray;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
+import android.view.GestureDetector;
 
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.text.TextBlock;
@@ -37,17 +43,19 @@ import java.util.ArrayList;
  * Created by Chandler on 12/19/2017.
  */
 
-/**implements View.OnClickListener*/
 
-public class BoxPickerActivity extends AppCompatActivity{
+public class BoxPickerActivity extends AppCompatActivity implements View.OnClickListener{
     private GraphicOverlay<OcrGraphic> mGraphicOverlay;
     private SparseArray<TextBlock> mInitialBlocks;
     private ArrayList<TextBlock> mSelectedBlocks;
     private TextBlockParser mParser;
     private ArrayList<Item> mItemList;
     private Bitmap mBitmap;
-    private ScannedImagePreview mImage;
+    private ImageView mImage;
     private FloatingActionButton mFAB;
+    private String TAG = "BoxPickerActivity";
+    private GestureDetector gestureDetector;
+
     public BoxPickerActivity() {
 
     }
@@ -59,35 +67,45 @@ public class BoxPickerActivity extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.testlinear);
+        setContentView(R.layout.activity_box_picking);
+        gestureDetector = new GestureDetector(this, new CaptureGestureListener());
         bindViews();
         init();
     }
 
     private void bindViews(){
-        mImage = (ScannedImagePreview) findViewById(R.id.parsedImage);
-        // mFAB = (FloatingActionButton) findViewById(R.id.finished);
+        mImage = (ImageView) findViewById(R.id.parsedImage);
+        mFAB = (FloatingActionButton) findViewById(R.id.finished);
         mGraphicOverlay = (GraphicOverlay<OcrGraphic>) findViewById(R.id.graphicOverlay);
     }
 
     private void init(){
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            try {
-                String extraString = extras.getString("Uri");
-                Log.i("String", extraString);
-                Uri uri = Uri.parse(extraString);
-                Log.i("init Uri", uri.toString());
-                mBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            mBitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.hbc_min);
+            /**
+             *  * TODO: Uncomment and figure out how to get full res bitmaps from the camera...
+             String extraString = extras.getString("Uri");
+                try {
+
+
+                    Log.i("String", extraString);
+                    Uri uri = Uri.parse(extraString);
+                    Log.i("init Uri", uri.toString());
+                    mBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+             **/
         } else {
             Log.e("Box init()", "Extras are null");
         }
 
-        //mImage.setImageBitmap(mBitmap);
-        //mFAB.setOnClickListener(this);
+        mImage.setImageBitmap(mBitmap);
+        mFAB.setOnClickListener(this);
         mParser = new TextBlockParser();
         mInitialBlocks = scanText(mBitmap);
         putGraphic(mInitialBlocks);
@@ -103,14 +121,20 @@ public class BoxPickerActivity extends AppCompatActivity{
             mGraphicOverlay.add(graphic);
         }
     }
-/**
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        boolean c = gestureDetector.onTouchEvent(e);
+        return c || super.onTouchEvent(e);
+    }
+
     public void onClick(View view){
         int viewId = view.getId();
         switch(viewId) {
             case R.id.finished:
                 Log.d("FAB","pressed");
                 if (mSelectedBlocks == null || mSelectedBlocks.size() > 2 || mSelectedBlocks.get(0).equals(mSelectedBlocks.get(1))) {
-                    Snackbar meSnackbar = Snackbar.make(findViewById(R.id.layout_receipt),
+                    Snackbar meSnackbar = Snackbar.make(findViewById(R.id.activity_box_picking),
                             "Please pick boxes containing your items", Snackbar.LENGTH_SHORT);
                     meSnackbar.show();
                 } else {
@@ -121,7 +145,6 @@ public class BoxPickerActivity extends AppCompatActivity{
                 }
         }
     }
-*/
     private SparseArray<TextBlock> scanText(Bitmap bitmap){
         TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
         SparseArray<TextBlock> items = null;
@@ -141,23 +164,43 @@ public class BoxPickerActivity extends AppCompatActivity{
         }
         return items;
     }
+
+    /**
+     * onTap is called to speak the tapped TextBlock, if any, out loud.
+     *
+     * @param rawX - the raw position of the tap
+     * @param rawY - the raw position of the tap.
+     * @return true if the tap was on a TextBlock
+     */
     private boolean onTap(float rawX, float rawY) {
-        // TODO: Speak the text when the user taps on screen.
         OcrGraphic graphic = mGraphicOverlay.getGraphicAtLocation(rawX, rawY);
         TextBlock text = null;
         if (graphic != null) {
             text = graphic.getTextBlock();
             if (text != null && text.getValue() != null) {
-                Log.d("Box clicked", "text data is being spoken! " + text.getValue());
-                // TODO: Speak the string.
+                Log.d(TAG, "text data is being spoken! " + text.getValue());
+                /*
+                *
+                * TODO: Should add to the ArrayList
+                *
+                */
             }
             else {
-                Log.d("Box clicked", "text data is null");
+                Log.d(TAG, "text data is null");
             }
         }
         else {
-            Log.d("Box clicked","no text detected");
+            Log.d(TAG,"no text detected");
         }
         return text != null;
     }
+
+    private class CaptureGestureListener extends GestureDetector.SimpleOnGestureListener {
+
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            return onTap(e.getRawX(), e.getRawY()) || super.onSingleTapConfirmed(e);
+        }
+    }
+
 }
