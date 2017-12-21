b65b1017418f470451e85f3c8de2e1c7898913f2
Chandler <chan4est@gmail.com>
2017-12-21 00:57:25 -0500

    Patchy but works, most of the time

--- app/src/main/java/com/whyyao/scanandsplit/UI/InteractiveReceiptActivity.java
+++ app/src/main/java/com/whyyao/scanandsplit/UI/InteractiveReceiptActivity.java
@@ -240,6 +240,13 @@ public class InteractiveReceiptActivity extends AppCompatActivity implements Vie
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
 }
 
 
